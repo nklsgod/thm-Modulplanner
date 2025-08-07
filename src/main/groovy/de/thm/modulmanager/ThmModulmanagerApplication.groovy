23d9813989809b09d5fc0fd8754a87135986d5fa
jnbn98 <jan.benn@mni.thm.de>
2025-08-08 00:24:49 +0200

    initial commit

---
+++ src/main/groovy/de/thm/modulmanager/ThmModulmanagerApplication.groovy
@@ -0,0 +1,13 @@
+package de.thm.modulmanager
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ThmModulmanagerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ThmModulmanagerApplication, args)
+    }
+
+}
---
+++ src/test/groovy/de/thm/modulmanager/ThmModulmanagerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package de.thm.modulmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThmModulmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
